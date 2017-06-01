383fc59b0db317ef1d4c4ed4da7d8dbbe5c9f10a
Yuhang Yang <Yuhang Yang>
2017-06-02 00:48:26 +0800

    第二次修改

--- yyh.sql
+++ yyh.sql
@@ -1,132 +1,197 @@
-create table major 
-(
-   majorname                  char(256)                      not null,
-   majorid                 integer                        not null,
-   constraint PK_major primary key (majorid)
-);
-
-create table assistant 
-(
-   id                   integer                        not null,
-   teacherid                 integer                        null,
-   name                   char(256)                      not null,
-   majorid                 integer                        null,
-   gender                   char(256)                      null,
-   grade                   integer                        null,
-   assistantid                 integer                        null,
-   constraint PK_assistant primary key clustered (id)
-);
-
-create table student 
-(
-   name                   char(256)                      not null,
-   id                   integer                        not null,
-   majorid                 integer                        null,
-   gender                   char(256)                      null,
-   grade                   integer                        null,
-   constraint PK_student primary key (id)
-);
-
-
-create table open 
-(
-   majorid                 integer                        not null,
-   courseid                 integer                        not null,
-   constraint PK_open primary key clustered (majorid, courseid)
-);
-
-
-create table teacher 
-(
-   name                   char(256)                      not null,
-   gender                   char(256)                      null,
-   post                  char(256)                      null,
-   teacherid                 integer                        not null,
-   constraint PK_teacher primary key (teacherid)
-);
-
 
 create table course 
 (
-   coursename                  char(256)                      not null,
-   courseid                 integer                        not null,
-   teacherid                 integer                        null,
-   hour                   integer                        null,
-   credit                   integer                        null,
+
+   coursename				   char(99)						 not null,
+
+   courseid					integer						   not null,
+
+   teacherid				 integer						null,
+
+   hour					  integer						 null,
+
+   credit					integer						   null,
+
    constraint PK_course primary key (courseid)
+
 );
+
+
+
 
 
 create table choose 
+
 (
-   id                   integer                        not null,
-   courseid                 integer                        not null,
-   coursegrade                 integer                        null,
+
+   id					integer						   not null,
+
+   courseid					integer						   not null,
+
+   coursegrade				   integer						  null,
+
    constraint PK_choose primary key clustered (id, courseid)
+
 );
-alter table assistant
-   add constraint FK1 foreign key (id)
-      references student (id)
-      on update restrict
-      on delete restrict;
 
 alter table assistant
-   add constraint FK2 foreign key ()
-      references teacher (id)
-      on update restrict
-      on delete restrict;
+
+   add constraint FK1 foreign key (id)
+
+	  references student (id)
+
+	  on update restrict
+
+	  on delete restrict;
+
+
+
+alter table assistant
+
+   add constraint FK2 foreign key (teacherid)
+
+	  references teacher (teacherid)
+
+	  on update restrict
+
+	  on delete restrict;
+
+
 
 alter table student
+
    add constraint FK3 foreign key (majorid)
-      references major (majorid)
-      on update restrict
-      on delete restrict;
 
-alter table open
+	  references major (majorid)
+
+	  on update restrict
+
+	  on delete restrict;
+
+
+
+alter table kaishe
+
    add constraint FK4 foreign key (majorid)
-      references major (majorid)
-      on update restrict
-      on delete restrict;
 
-alter table open
+	  references major (majorid)
+
+	  on update restrict
+
+	  on delete restrict;
+
+
+
+alter table kaishe
+
    add constraint FK5 foreign key (courseid)
-      references course (courseid)
-      on update restrict
-      on delete restrict;
+
+	  references course (courseid)
+
+	  on update restrict
+
+	  on delete restrict;
+
+
 
 alter table course
+
    add constraint FK6 foreign key (teacherid)
-      references teacher (teacherid)
-      on update restrict
-      on delete restrict;
 
+	  references teacher (teacherid)
+
+	  on update restrict
+
+	  on delete restrict;
 alter table choose
+
    add constraint FK7 foreign key (id)
-      references student (id)
-      on update restrict
-      on delete restrict;
+
+	  references student (id)
+
+	  on update restrict
+
+	  on delete restrict;
+
+
 
 alter table choose
+
    add constraint FK8 foreign key (courseid)
-      references course (courseid)
-      on update restrict
-      on delete restrict;
+
+	  references course (courseid)
+
+	  on update restrict
+
+	  on delete restrict;
+
 delimiter //
-create trigger stutrigger
- after update on student(id)
+
+create trigger stutrigger1
+
+ after update on student
+
  for each row
+
  begin
+ if new.id!= old.id then
+
  update choose
- set id=i.id
- from choose br,deleted d,inserted i
- where br.id=d.id;
+
+ set id=new.id
+
+ where choose.id=old.id;
+ 
+ end if;
+ 
  end//
+ 
 delimiter //
-create trigger coursetrigger
- after update on course(courseid)
+
+create trigger stutrigger2
+
+after delete on student
+
+for	 each  row
+
+begin
+
+delete from choose where choose.id=old.id;
+
+end//
+
+delimiter //
+
+create trigger coursetrigger1
+
+ after update on course
+ 
+ for each row
+
  begin
- update open
- set courseid=i.courseid
- from open br,deleted d,inserted i
- where br.courseid=d.courseid;
+
+if new.courseid!=old.courseid then
+
+ update kaishe
+
+ set courseid=new.courseid
+
+where	kaishe.courseid=old.courseid;
+
+end if;
+
  end//
+
+delimiter //
+
+create trigger coursetrigger2
+
+after delete on course
+
+for	 each row 
+
+begin
+
+delete from kaishe where kaishe.courseid=old.courseid;
+
+end//
